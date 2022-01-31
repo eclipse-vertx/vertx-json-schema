dd4925c3b93562a707d8167cf369ad8e1ad93c14
Adrian Floarea <adrianfloarea07@yahoo.com>
2022-01-31 20:59:05 +0200

    Implement exact arithmetic for 'multipleOf' validation
    Signed-off-by: Adrian Floarea <adrianfloarea07@yahoo.com>

--- src/main/java/io/vertx/json/schema/common/MultipleOfValidatorFactory.java
+++ src/main/java/io/vertx/json/schema/common/MultipleOfValidatorFactory.java
@@ -15,13 +15,15 @@ import io.vertx.core.json.pointer.JsonPointer;
 import io.vertx.json.schema.SchemaException;
 import io.vertx.json.schema.ValidationException;
 
+import java.math.BigDecimal;
+
 public class MultipleOfValidatorFactory implements ValidatorFactory {
 
   @Override
   public Validator createValidator(JsonObject schema, JsonPointer scope, SchemaParserInternal parser, MutableStateValidator parent) {
     try {
       Number multipleOf = (Number) schema.getValue("multipleOf");
-      return new MultipleOfValidator(multipleOf.doubleValue());
+      return new MultipleOfValidator(new BigDecimal(multipleOf.toString()));
     } catch (ClassCastException e) {
       throw new SchemaException(schema, "Wrong type for multipleOf keyword", e);
     } catch (NullPointerException e) {
@@ -35,16 +37,16 @@ public class MultipleOfValidatorFactory implements ValidatorFactory {
   }
 
   static class MultipleOfValidator extends BaseSyncValidator {
-    private final double multipleOf;
+    private final BigDecimal multipleOf;
 
-    public MultipleOfValidator(double multipleOf) {
+    public MultipleOfValidator(BigDecimal multipleOf) {
       this.multipleOf = multipleOf;
     }
 
     @Override
     public void validateSync(ValidatorContext context, Object in) throws ValidationException {
       if (in instanceof Number) {
-        if (((Number) in).doubleValue() % multipleOf != 0) {
+        if (new BigDecimal(in.toString()).remainder(multipleOf).compareTo(BigDecimal.ZERO) != 0) {
           throw ValidationException.create("provided number should be multiple of " + multipleOf, "multipleOf", in);
         }
       }
