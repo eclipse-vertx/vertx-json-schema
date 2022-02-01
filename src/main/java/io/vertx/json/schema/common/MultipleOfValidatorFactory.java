a818b6e9db4ec598d979b75ca54b8209f5d157fe
Paulo Lopes <pmlopes@gmail.com>
2022-02-01 14:24:33 +0100

    Fix #53: Backporting from the 2020-12 branch the fix for multipleOf
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- src/main/java/io/vertx/json/schema/common/MultipleOfValidatorFactory.java
+++ src/main/java/io/vertx/json/schema/common/MultipleOfValidatorFactory.java
@@ -15,13 +15,15 @@ import io.vertx.core.json.pointer.JsonPointer;
 import io.vertx.json.schema.SchemaException;
 import io.vertx.json.schema.ValidationException;
 
+import java.math.BigDecimal;
+
 public class MultipleOfValidatorFactory implements ValidatorFactory {
 
   @Override
   public Validator createValidator(JsonObject schema, JsonPointer scope, SchemaParserInternal parser, MutableStateValidator parent) {
     try {
       Number multipleOf = (Number) schema.getValue("multipleOf");
-      return new MultipleOfValidator(multipleOf.doubleValue());
+      return new MultipleOfValidator(multipleOf);
     } catch (ClassCastException e) {
       throw new SchemaException(schema, "Wrong type for multipleOf keyword", e);
     } catch (NullPointerException e) {
@@ -35,17 +37,50 @@ public class MultipleOfValidatorFactory implements ValidatorFactory {
   }
 
   static class MultipleOfValidator extends BaseSyncValidator {
-    private final double multipleOf;
+    private final Long multipleOfL;
+    private final BigDecimal multipleOfD;
 
-    public MultipleOfValidator(double multipleOf) {
-      this.multipleOf = multipleOf;
+    public MultipleOfValidator(Number multipleOf) {
+      // can be null to signal integer arithmetic
+      multipleOfD = toBigDecimal(multipleOf, false);
+      if (multipleOfD == null) {
+        multipleOfL = multipleOf.longValue();
+      } else {
+        multipleOfL = null;
+      }
+    }
+
+    private BigDecimal toBigDecimal(Number in, boolean force) {
+      if (in instanceof BigDecimal) {
+        return (BigDecimal) in;
+      }
+      if (in instanceof Float) {
+        return BigDecimal.valueOf(in.floatValue());
+      }
+      if (in instanceof Double) {
+        return BigDecimal.valueOf(in.doubleValue());
+      }
+      if (force) {
+        return BigDecimal.valueOf(in.longValue());
+      }
+      return null;
     }
 
     @Override
     public void validateSync(ValidatorContext context, Object in) throws ValidationException {
       if (in instanceof Number) {
-        if (((Number) in).doubleValue() % multipleOf != 0) {
-          throw ValidationException.create("provided number should be multiple of " + multipleOf, "multipleOf", in);
+        // floating point arithmetic,
+        // if the multipleOf is decimal, we always need to handle this operation as decimal
+        final BigDecimal inBD = toBigDecimal((Number) in, multipleOfD != null);
+        if (inBD != null) {
+          if (inBD.remainder(multipleOfD).compareTo(BigDecimal.ZERO) != 0) {
+            throw ValidationException.create("provided number should be multiple of " + multipleOfD, "multipleOf", in);
+          }
+        } else {
+          // integer arithmetic, fallback for simpler arithmetic
+          if (((Number) in).longValue() % multipleOfL != 0L) {
+            throw ValidationException.create("provided number should be multiple of " + multipleOfL, "multipleOf", in);
+          }
         }
       }
     }
