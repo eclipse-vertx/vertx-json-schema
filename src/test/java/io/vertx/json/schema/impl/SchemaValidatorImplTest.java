973f2c9841384560a0e5f7f6d6440aebf70d9220
Gerrett <Gerrett.Pape@colorado.edu>
2024-01-08 21:30:08 -0600

    Adding format testing unit tests.
    Signed-off-by: Gerrett <Gerrett.Pape@colorado.edu>

--- src/test/java/io/vertx/json/schema/impl/SchemaValidatorImplTest.java
+++ src/test/java/io/vertx/json/schema/impl/SchemaValidatorImplTest.java
@@ -1,17 +1,21 @@
 package io.vertx.json.schema.impl;
 
+import io.vertx.core.json.JsonObject;
 import io.vertx.json.schema.JsonSchema;
 import io.vertx.json.schema.JsonSchemaOptions;
 import io.vertx.json.schema.common.dsl.Keywords;
+import io.vertx.json.schema.common.dsl.NumberKeyword;
 import io.vertx.json.schema.common.dsl.StringFormat;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.stream.Stream;
 
 import static io.vertx.json.schema.Draft.DRAFT201909;
-import static io.vertx.json.schema.common.dsl.Schemas.stringSchema;
+import static io.vertx.json.schema.common.dsl.Schemas.*;
 import static io.vertx.json.schema.common.dsl.StringFormat.BYTE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,6 +33,19 @@ class SchemaValidatorImplTest {
     );
   }
 
+  static Stream<Arguments> testNumberSchema() {
+    return Stream.of(
+      Arguments.of("Invalid Double", numberSchema().toJson().put("format", "double"), new BigDecimal("99999999.999"), false),
+      Arguments.of("Invalid Float", numberSchema().toJson().put("format", "float"), Double.MAX_VALUE, false),
+      Arguments.of("Invalid int32", intSchema().toJson().put("format", "int32"), Long.MAX_VALUE, false),
+      Arguments.of("Invalid int64", intSchema().toJson().put("format", "int64"), new BigInteger("99999999999999999999999"), false),
+      Arguments.of("Valid Double", numberSchema().toJson().put("format", "double"), Double.MAX_VALUE, true),
+      Arguments.of("Valid Float", numberSchema().toJson().put("format", "float"), Float.MAX_VALUE, true),
+      Arguments.of("Valid int32", intSchema().toJson().put("format", "int32"), Integer.MAX_VALUE, true),
+      Arguments.of("Valid int64", intSchema().toJson().put("format", "int64"), Long.MAX_VALUE, true)
+    );
+  }
+
   @ParameterizedTest(name = "{index} With keyword format: {0} and value {1}")
   @MethodSource
   void testStringSchema(StringFormat formatValue, Object value, boolean isValid) {
@@ -36,4 +53,13 @@ class SchemaValidatorImplTest {
     SchemaValidatorImpl validator = new SchemaValidatorImpl(schema, DUMMY_OPTIONS);
     assertThat(validator.validate(value).getValid()).isEqualTo(isValid);
   }
+
+  @ParameterizedTest(name = "{0} with the value {2}")
+  @MethodSource
+  void testNumberSchema(String keyword, JsonObject objSchema, Object value, boolean isValid) {
+    JsonSchema schema = JsonSchema.of(objSchema);
+    SchemaValidatorImpl validator = new SchemaValidatorImpl(schema, DUMMY_OPTIONS);
+    assertThat(validator.validate(value).getValid()).isEqualTo(isValid);
+  }
+
 }
